7fa735bcae776aed7edc84b20487a19f429b89c7
Mr-MeowMeow <satyanarayanprashar73@gmail.com>
2022-05-26 22:26:03 +0530

    .

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -34,62 +34,64 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [
-//
-          Container(
-            height: 104,
-            width: size.width,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 155, 231, 157),
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40),
-                  bottomRight: Radius.circular(40)),
-            ),
+      body: SafeArea(
+        child: Column(
+          children: [
             //
-            child: Column(
-              children: [
-                const SizedBox(height: 32),
-                Row(children: [
-                  //
+            Container(
+              height: 104,
+              width: size.width,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 155, 231, 157),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40)),
+              ),
+              //
+              child: Column(
+                children: [
+                  const SizedBox(height: 32),
+                  Row(children: [
+                    //
 
-                  CupertinoButton(
-                    onPressed: () {
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //       builder: (context) => const Profile_Page()),
-                      // );
-                    },
-                    child: const CircleAvatar(
-                      child: Icon(Icons.person),
+                    CupertinoButton(
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //       builder: (context) => const Profile_Page()),
+                        // );
+                      },
+                      child: const CircleAvatar(
+                        child: Icon(Icons.person),
+                      ),
                     ),
-                  ),
 
-                  //
-                  Spacer(),
-                  //
-                  CupertinoButton(
-                    child: Icon(
-                      Icons.search,
-                      size: 32,
+                    //
+                    Spacer(),
+                    //
+                    CupertinoButton(
+                      child: Icon(
+                        Icons.search,
+                        size: 32,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Search_Page(
+                                  firebaseUser: widget.firebaseUser,
+                                  userModel: widget.userModel)),
+                        );
+                      },
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Search_Page(
-                                firebaseUser: widget.firebaseUser,
-                                userModel: widget.userModel)),
-                      );
-                    },
-                  ),
-                ]),
-              ],
-            ),
-          )
-//
-        ],
+                  ]),
+                ],
+              ),
+            )
+            //
+          ],
+        ),
       ),
     );
   }
